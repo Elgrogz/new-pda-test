78f3417ddaad352f2bed665e6dbe1e61183bebb9
Elgrogz <gregorgilchrist@gmail.com>
2017-03-12 13:36:27 +0000

    negatives and decimals tested

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -11,9 +11,76 @@ describe('calculator functionality', function() {
 
   // write integration tests here in the form of "it should do something..."
   it('should have working number buttons', function(){
-    running_total = element(by.css('#running_total'))
+    running_total = element(by.css('#running_total'));
     element(by.css('#number2')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
+
+  it('should add correctly', function(){
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('6')
+  });
+
+  it('should subtract correctly', function(){
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number7')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('3')
+  });
+
+  it('should multiply correctly', function(){
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number2')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('8')
+  });
+
+  it('should divide correctly', function(){
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number8')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('2')
+  });
+
+  it('should be able to chain multiple operators together', function() {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number8')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number6')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('30');
+  });  
+  
+  it('should be able to have negative numbers', function() {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number3')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number7')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-4');
+  });
+
+  it('should be able to have decimal points in numbers', function() {
+    running_total = element(by.css('#running_total'));
+    element(by.css('#number1')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('0.25');
+  });
 
 });
